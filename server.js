873c85002e588f0662bde4fd1292118169fdb952
Victor Onyebuchi <victorchike247@gmail.com>
2021-06-27 08:33:23 +0100

    added endpoint

--- server.js
+++ server.js
@@ -13,7 +13,10 @@ app.get('/', function (req, res) {
     res.sendFile(process.cwd() + '/views/index.html');
 });
 
+app.post('/api/fileanalyse', upload.single('upfile'), (req, res) => {
 
+  
+});
 
 
 const port = process.env.PORT || 3000;
